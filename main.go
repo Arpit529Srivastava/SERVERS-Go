0df8ff07effd7acdf497eb420a78161d25e605d3
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-07-30 03:39:20 +0530

    started the server

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Server Started:")
+	if err:= http.ListenAndServe(":8080", err); err!=nil{
+		log.Fatal(err)
+	}
 }
