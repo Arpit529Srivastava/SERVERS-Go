c0a119addf3ef77a843e95fb2483b546713bf3f7
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-07-30 03:44:26 +0530

    added the handler function

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"log"
 	"net/http"
 )
-
+func hellohandler(w http.ResponseWriter, r *http.Request ){
+	if r.URL.Path!="/hello"{
+		http.Error(w,"status not found", http.StatusNotFound)
+	}
+	return
+}
 func main() {
 	fmt.Println("Server Started:")
 	if err:= http.ListenAndServe(":8080", err); err!=nil{
